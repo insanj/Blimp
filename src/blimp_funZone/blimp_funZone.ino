b87d31c5f8975b72206f90c213dd8ba64e2447cc
insanj <insanj@gmail.com>
2014-05-08 16:49:40 -0400

    it's alive!

--- src/blimp_funZone/blimp_funZone.ino
+++ src/blimp_funZone/blimp_funZone.ino
@@ -1,7 +1,8 @@
 #define MAX_POWER 255
 #define MIN_POWER 0
-#define WAIT_DELAY 1500
+#define WAIT_DELAY 3500
 #define TOLERANCE 10
+#define lol true
 
 int ar = 1; // analog
 int ctrl = 7; // analog
@@ -25,48 +26,57 @@ void setup() {
 // is within a certain tolerance range, then powers the motor accordingly
 // for the WAIT_DELAY ms, then does it again.
 void loop() {
-  Serial.println("getting measurement data");
-  analogWrite(ctrl, MAX_POWER); 
-  
-  delay(20); // 16.5ms ± 3.7ms
-  int sensorValue = analogRead(ar); // first measurement
-  
-  delay(20);
-  int sensorValue2 = analogRead(ar); // second measurement
-
-  int avg = (sensorValue + sensorValue2) / 2; // average measurement
-  Serial.println(avg, DEC);
-  
-  int delayAmount = WAIT_DELAY;
-  if (range > (avg + TOLERANCE) || range < (avg - TOLERANCE)) {
-    range = avg;
+  if (lol) {
+    engage(lol);
   }
   
-  // forwards
-  if (avg < range) {
-    Serial.println("moving forwards");
-    analogWrite(fmotor1, MAX_POWER);
-    analogWrite(bmotor1, MIN_POWER);
-    
-    analogWrite(fmotor2, MAX_POWER);
-    analogWrite(bmotor2, MIN_POWER);
-    
-    analogWrite(fmotor3, MAX_POWER);
-    analogWrite(bmotor3, MIN_POWER);
-  }
-  
-  // backwards
   else {
-    Serial.println("moving backwards");
-    analogWrite(fmotor1, MIN_POWER);
-    analogWrite(bmotor1, MAX_POWER);
+    Serial.println("getting measurement data");
+    analogWrite(ctrl, MAX_POWER); 
     
-    analogWrite(fmotor2, MIN_POWER);
-    analogWrite(bmotor2, MAX_POWER);
+    delay(20); // 16.5ms ± 3.7ms
+    int sensorValue = analogRead(ar); // first measurement
     
-    analogWrite(fmotor3, MIN_POWER);
-    analogWrite(bmotor3, MAX_POWER);
-  }  
+    delay(20);
+    int sensorValue2 = analogRead(ar); // second measurement
   
-  delay(delayAmount);
+    int avg = (sensorValue + sensorValue2) / 2; // average measurement
+    Serial.println(avg, DEC);
+    
+    int delayAmount = WAIT_DELAY;
+    if (range > (avg + TOLERANCE) || range < (avg - TOLERANCE)) {
+      range = avg;
+    }
+    
+    // forwards
+    if (avg < range) {
+      Serial.println("moving forwards");
+       engage(true);
+    }
+    
+    // backwards
+    else {
+      Serial.println("moving backwards");
+      engage(false);
+    }
+    
+    delay(delayAmount);
+  }
+}
+
+void engage (boolean forward) {
+  int on_power = MAX_POWER, off_power = 0;
+  if (!forward) {
+    on_power = 0;
+    off_power = MAX_POWER;
+  }
+  
+  analogWrite(fmotor1, on_power);
+  analogWrite(bmotor1, off_power);
+  
+  analogWrite(fmotor2, on_power);
+  analogWrite(bmotor2, off_power);
+  
+  analogWrite(fmotor3, on_power);
+  analogWrite(bmotor3, off_power);
 }
